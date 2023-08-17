a3a862f7d0794e83f64b6416063c06572d6f7807
Lebohang Sopazi <72252063+DaLebza10111@users.noreply.github.com>
2023-08-17 14:55:02 +0200

    remove comments

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -2,10 +2,6 @@
 using System;
 using System.Collections.Generic;
 
-// Please note - THIS IS A BAD APPLICATION - DO NOT REPLICATE WHAT IT DOES
-// This application was designed to simulate a poorly-built application that
-// you need to support. Do not follow any of these practices. This is for 
-// demonstration purposes only. You have been warned.
 namespace ConsoleUI
 {
     class Program
