f40b3b277c1b1b38fe41834b7cfe096777deea32
Lebohang Sopazi <72252063+DaLebza10111@users.noreply.github.com>
2023-08-17 14:11:43 +0200

    Replace long code with method call

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -14,64 +14,10 @@ namespace ConsoleUI
             string w, rawTimeWorked;
             int i;
             double ttl, t;
-            List<TimeSheetEntry> ents = new List<TimeSheetEntry>();
-            Console.Write("Enter what you did: ");
-            w = Console.ReadLine();
-            Console.Write("How long did you do it for: ");
-            rawTimeWorked = Console.ReadLine();
 
-            while (double.TryParse(rawTimeWorked, out t) == false)
-            {
-                Console.WriteLine();
-                Console.WriteLine("Invalid number given");
-                Console.Write("How long did you do it for: ");
-                rawTimeWorked = Console.ReadLine();
-            }
+            List<TimeSheetEntry> ents = LoadTimesheets();
 
-            TimeSheetEntry ent = new TimeSheetEntry();
-            ent.HoursWorked = t;
-            ent.WorkDone = w;
-            ents.Add(ent);
-            Console.Write("Do you want to enter more time (yes/no): ");
-
-            string answer = Console.ReadLine();
-            bool cont = false;
-
-            if (answer.ToLower() == "yes")
-            {
-                cont = true;
-            }
-
-            while (cont == true)
-            {
-                Console.Write("Enter what you did: ");
-                w = Console.ReadLine();
-                Console.Write("How long did you do it for: ");
-                rawTimeWorked = Console.ReadLine();
-
-                while (double.TryParse(rawTimeWorked, out t) == false)
-                {
-                    Console.WriteLine();
-                    Console.WriteLine("Invalid number given");
-                    Console.Write("How long did you do it for: ");
-                    rawTimeWorked = Console.ReadLine();
-                }
-
-                ent = new TimeSheetEntry();
-                ent.HoursWorked = t;
-                ent.WorkDone = w;
-                ents.Add(ent);
-
-                Console.Write("Do you want to enter more time (yes/no): ");
-                answer = Console.ReadLine();
-                cont = false;
-
-                if (answer.ToLower() == "yes")
-                {
-                    cont = true;
-                }
-            } 
-            ttl = 0;
+			ttl = 0;
             for (i = 0; i < ents.Count; i++)
             {
                 if (ents[i].WorkDone.ToLower().Contains("acme"))
